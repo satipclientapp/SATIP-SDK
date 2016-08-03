a6cac7974cabaa99820f5bf87402853310157298
Felix Paul Kühne <fkuehne@videolan.org>
2016-08-03 18:53:39 +0200

    Restart discovery when app becomes active again

--- VLC-tvOS-Discoverer/AppDelegate.m
+++ VLC-tvOS-Discoverer/AppDelegate.m
@@ -100,8 +100,9 @@
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    [[SESServerDiscoveryController sharedDiscoveryController] startDiscovery];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
